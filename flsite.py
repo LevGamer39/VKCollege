bbdaf6054a394e375e221fa03d535f321bcdd24f
Лев <kirillov.lev@outlook.com>
2025-08-09 11:29:13 +0200

    1

--- flsite.py
+++ flsite.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, g, request, request_started, url_for, redirect, flash, session
 
-app = Flask("VKCollege")
+app = Flask("0")
 app.config.update(DEBUG=True)
 
 menu = [
@@ -29,6 +29,9 @@ def index():
 @app.errorhandler(404)
 def error_404(error): 
     return render_template("error_404.html", menu=menu)
+
+
+
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=4221, debug=app.config.get("DEBUG"))
