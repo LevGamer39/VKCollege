3f1febb29966960eb3f8731fb990f86b9a8637db
Polysock <aleksandr30032000@gmail.com>
2025-08-09 13:12:30 +0200

    1

--- flsite.py
+++ flsite.py
@@ -1,15 +1,12 @@
 from flask import Flask, render_template, g, request, request_started, url_for, redirect, flash, session
 from SECRET import web_app_secret_key, web_app_debug
 
-
 app = Flask("0")
 app.config.update(SECRET_KEY=web_app_secret_key)
 app.config.update(DEBUG=web_app_debug)
+
 menu = [
-        {
-            "title": "Главная",
-            "url": "/home"
-        }
+    {"title": "Главная", "url": "/home"}
 ]
 
 @app.route("/")
@@ -28,9 +25,6 @@ def register():
 @app.errorhandler(404)
 def error_404(error): 
     return render_template("error_404.html", menu=menu)
-
-
-
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=4221, debug=app.config.get("DEBUG"))
