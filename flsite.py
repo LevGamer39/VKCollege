143c0a2e23f65bd2ddc809dd09fa37450c33f085
Лев <kirillov.lev@outlook.com>
2025-08-09 12:58:22 +0200

    1

--- flsite.py
+++ flsite.py
@@ -21,7 +21,7 @@ def index():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        flash('Извините, но мест больше нет ')
+        flash('Извините, но мест больше нет')
         return redirect(url_for('register'))
     return render_template('register.html', title='Регистрация', menu=menu)
 
