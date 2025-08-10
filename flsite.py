688250f46bef9ad2216a562de1b6cab9dc6562c6
Лев <kirillov.lev@outlook.com>
2025-08-10 19:41:52 +0200

    1

--- flsite.py
+++ flsite.py
@@ -1,30 +1,22 @@
 from flask import Flask, render_template, g, request, request_started, url_for, redirect, flash, session
-from SECRET import web_app_secret_key, web_app_debug
+from SECRET import web_app_secret_key
 
-app = Flask("0")
+application = Flask(__name__)
+
 app.config.update(SECRET_KEY=web_app_secret_key)
-app.config.update(DEBUG=web_app_debug)
 
-menu = [
-    {"title": "Главная", "url": "/home"}
-]
-
-@app.route("/")
-@app.route("/home")
-@app.route("/index")
+@application.route("/")
+@application.route("/home")
+@application.route("/index")
 def index():
-    return render_template("index.html", menu=menu)
+    return render_template("index.html")
 
-@app.route('/register', methods=['GET', 'POST'])
+@application.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         flash('Извините, но мест больше нет')
         return redirect(url_for('register'))
-    return render_template('register.html', title='Регистрация', menu=menu)
-
-@app.errorhandler(404)
-def error_404(error): 
-    return render_template("error_404.html", menu=menu)
+    return render_template('register.html', title='Регистрация')
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=4221, debug=app.config.get("DEBUG"))
+   application.run(host='0.0.0.0')
