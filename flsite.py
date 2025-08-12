446f583df8b41d0267d30a751c1b211311cb3670
LevGamer39 <Levk039@yandex.ru>
2025-08-12 17:08:28 +0200

    modified:   flsite.py

--- flsite.py
+++ flsite.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, url_for, redirect, flash
-from SECRET import web_app_secret_key, web_app_debug
+from SECRET import web_app_secret_key
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 
-app = Flask(
+application = Flask(
     __name__,
     template_folder=str(BASE_DIR / "templates"),
     static_folder=str(BASE_DIR / "static")
@@ -15,7 +15,7 @@ MAINTENANCE_MODE = False # переключатель техобслуживан
 
 
 # Конфиг
-app.config.update(SECRET_KEY=web_app_secret_key, DEBUG=web_app_debug)
+application.config.update(SECRET_KEY=web_app_secret_key)
 
 # Меню
 menu = [
@@ -28,7 +28,7 @@ menu = [
 ]
 
 
-@app.before_request
+@application.before_request
 def check_maintenance():
     # Если включен режим техобслуживания и пользователь не на странице /maintenance
     if MAINTENANCE_MODE:
@@ -37,35 +37,35 @@ def check_maintenance():
             return redirect(url_for('maintenance'))
 
 
-@app.route('/technicalwork')
+@application.route('/technicalwork')
 def maintenance():
     return render_template('technicalwork.html')  # HTML с сообщением «Сайт на техобслуживании»
 
 
 # --- Маршруты ---
-@app.route("/")
-@app.route("/home")
-@app.route("/index")
+@application.route("/")
+@application.route("/home")
+@application.route("/index")
 def index():
     return render_template("index.html", title="Главная", menu=menu)
 
-@app.route("/about")
+@application.route("/about")
 def about():
     return render_template("about.html", title="О колледже", menu=menu)
 
-@app.route("/programs")
+@application.route("/programs")
 def programs():
     return render_template("programs.html", title="Направления обучения", menu=menu)
 
-@app.route("/contacts")
+@application.route("/contacts")
 def contacts():
     return render_template("contacts.html", title="Контакты", menu=menu)
 
-@app.route("/news")
+@application.route("/news")
 def news():
     return render_template("news.html", title="Новости", menu=menu)
 
-@app.route("/register", methods=["GET", "POST"])
+@application.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
         flash("Извините, но мест больше нет")
@@ -73,16 +73,15 @@ def register():
     return render_template("register.html", title="Регистрация", menu=menu)
 
 # Обработка ошибки 404
-@app.errorhandler(404)
+@application.errorhandler(404)
 def error_404(error):
     return render_template("error_404.html", title="Страница не найдена", menu=menu), 404
 
 # Для Passenger (хостинг)
-application = app
 
 # Для локального запуска
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=4221, debug=app.config.get("DEBUG"))
+    application.run(host='0.0.0.0')
 
 
 
